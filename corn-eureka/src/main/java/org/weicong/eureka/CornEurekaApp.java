64abd6ba3b53694d6db9210bbac5a2ee541fce4d
weicong <weicongmail@foxmail.com>
2019-05-29 18:51:43 +0800

    add corn-eureka

---
+++ corn-eureka/src/main/java/org/weicong/eureka/CornEurekaApp.java
@@ -0,0 +1,20 @@
+package org.weicong.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @description 服务中心
+ * @author weicong
+ * @date 2019年5月29日 下午6:36:48
+ * @version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class CornEurekaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornEurekaApp.class, args);
+	}
+}
