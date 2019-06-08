3f6f524d60eaa2caab7cd0620e9491f23920521a
weicong <weicongmail@foxmail.com>
2019-06-08 11:47:51 +0800

    add corn-uas

---
+++ corn-uas/corn-uas-biz/src/main/java/org/weicong/uas/CornUasApp.java
@@ -0,0 +1,21 @@
+package org.weicong.uas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @description 
+ * @author weicong
+ * @time   2019年6月8日 上午11:42:59
+ * @version 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CornUasApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornUasApp.class, args);
+	}
+	
+}
